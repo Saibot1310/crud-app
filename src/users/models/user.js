3336d2d488b153d26c8576c94966c9921296a050
Tobias Vega <tobivega1310@gmail.com>
2026-09-02 19:01:13 -0300

    fix: corregir propiedades con camelCase

--- src/users/models/user.js
+++ src/users/models/user.js
@@ -8,16 +8,16 @@ export class User {
     isActive,
     balance,
     avatar,
-    first_name,
-    last_name,
+    firstName,
+    lastName,
     gender,
   }) {
     this.id = id;
     this.isActive = isActive;
     this.balance = balance;
     this.avatar = avatar;
-    this.first_name = first_name;
-    this.last_name = last_name;
+    this.firstName = firstName;
+    this.lastName = lastName;
     this.gender = gender;
   }
 }
