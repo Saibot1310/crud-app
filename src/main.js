7343d3e624f6f411ed3e308f4286bba50ca96c4c
Tobias Vega <tobivega1310@gmail.com>
2026-09-02 18:32:11 -0300

    feat: inicializar UsersApp

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import './style.css'
+import { UsersApp } from './users/users-app';
 
 document.querySelector('#app').innerHTML = `
-
+  <div class="card"></div>
 `;
+
+const element = document.querySelector('.card');
+
+await UsersApp(element);
