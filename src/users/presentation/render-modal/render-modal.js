86c8639f508337c8eb79fd819f4808eddb5402e8
Tobias Vega <tobivega1310@gmail.com>
2026-09-04 12:59:49 -0300

    feat: añadir renderizado de modal y evento del formulario

--- src/users/presentation/render-modal/render-modal.js
+++ src/users/presentation/render-modal/render-modal.js
@@ -1,6 +1,14 @@
 import modalHtml from './render-modal.html?raw';
 import './render-modal.css';
-let modal;
+let modal, form;
+
+export const showModal = () => {
+  modal?.classList.remove('hide-modal')
+}
+
+export const hideModal = () => {
+  modal?.classList.add('hide-modal');
+}
 
 /**
  * 
@@ -13,6 +21,17 @@ export const renderModal = (element) => {
   modal = document.createElement('div');
   modal.innerHTML = modalHtml;
   modal.classList.add('modal-container', 'hide-modal');
+  form = modal.querySelector('form');
+
+  modal.addEventListener('click', (event) => {
+    if (event.target.className === 'modal-container') {
+      hideModal();
+    }
+  });
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+  })
 
   element.append(modal);
 
