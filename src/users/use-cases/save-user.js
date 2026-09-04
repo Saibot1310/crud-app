d7d56413b97e4fde46762bb65bd6ad84e03c6efa
Tobias Vega <tobivega1310@gmail.com>
2026-09-04 18:25:25 -0300

    feat: validar campos vacíos al crear usuario

--- src/users/use-cases/save-user.js
+++ src/users/use-cases/save-user.js
@@ -1,3 +1,4 @@
+import { userModelToLocalhost } from "../mappers/user-to-localhost.mapper";
 import { User } from "../models/user";
 
 /**
@@ -8,11 +9,17 @@ export const saveUser = async (userLike) => {
 
   const user = new User(userLike);
 
+  if (!user.firstName || !user.lastName) {
+    throw 'First & last name are required';
+  }
+
+  const userToSave = userModelToLocalhost(user);
+
   if (user.id) {
     throw 'No implementada';
   }
 
-  const updatedUser = await createUser(user)
+  const updatedUser = await createUser(userToSave);
   return updatedUser;
 }
 
